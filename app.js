88bb05ee9d3fe0bb9a09633667d67b8b99e83bfa
Mentor Palokaj <mentor@palokaj.co>
2017-12-26 16:04:31 +0100

    last bvits

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const verbose = false
 const crashintensity = 50
 
 
-console.log( `Crash intensity: ${crashintensity}%, market return ${marketreturn}%\n` )
+console.log( `# Assumptions\nCrash intensity: ${crashintensity}%, market return ${marketreturn}%\n` )
+console.log( '# Readme\nChunk investing means you invest everything right now.\nDCA means you invest a certain amount per year.\nDCA assumes you invest principal/year amount in a linear way.\n' )
 
 for (let crashyear = 1; crashyear < horizon+1; crashyear++) {
 	let result = compare( marketreturn, horizon, crashyear, crashintensity, principal, verbose )
-	console.log( `Recession year ${crashyear} | Chunk ${ result.chunkroirec < 0 ? String( result.chunkroirec ).red : String( result.chunkroirec ).green }%` )
+	console.log( `Recession year ${ crashyear < 10 ? ( "0" + String( crashyear ) ) : crashyear} | Chunk ROI vs DCA ${ result.chunkroirec < 0 ? String( result.chunkroirec ).red : String( result.chunkroirec ).green }%` )
 }
